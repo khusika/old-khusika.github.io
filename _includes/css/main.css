4d1d16fff10f09f0a512a9823af551f3a23f8d0e
khusika <khusikadhamar@gmail.com>
2017-07-07 21:35:35 +0700

    freelancer -> khusika

--- _includes/css/main.css
+++ _includes/css/main.css
@@ -1,5 +1,5 @@
 /*!
- * Start Bootstrap - Freelancer Bootstrap Theme (http://startbootstrap.com)
+ * khusika.github.io
  * Code licensed under the Apache License v2.0.
  * For details, see http://www.apache.org/licenses/LICENSE-2.0.
  */
