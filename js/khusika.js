1dfb5ca07a7b299de053a5a8bcbf11fe8eb196a3
khusika <khusikadhamar@gmail.com>
2017-12-27 18:43:56 +0700

    Filter category in Project page
    Adapted from https://bootsnipp.com/snippets/featured/portfolio-gallery-with-filtering-category

--- js/khusika.js
+++ js/khusika.js
@@ -35,3 +35,30 @@ $('body').scrollspy({
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
+
+$(document).ready(function(){
+
+    $(".filter-button").click(function(){
+        var value = $(this).attr('data-filter');
+
+        if(value == "all")
+        {
+            //$('.filter').removeClass('hidden');
+            $('.filter').show('1000');
+        }
+        else
+        {
+//            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
+//            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+            $(".filter").not('.'+value).hide('3000');
+            $('.filter').filter('.'+value).show('3000');
+
+        }
+    });
+
+    if ($(".filter-button").removeClass("active")) {
+$(this).removeClass("active");
+}
+$(this).addClass("active");
+
+});
