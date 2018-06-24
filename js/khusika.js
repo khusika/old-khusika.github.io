506e2475d33a8758f716de007d6c97d2c3f5fb58
khusika <khusikadhamar@gmail.com>
2018-06-24 14:22:38 +0700

    Revert "Filter category in Project page"
    This reverts commit 1dfb5ca07a7b299de053a5a8bcbf11fe8eb196a3.
    
    Signed-off-by: khusika <khusikadhamar@gmail.com>

--- js/khusika.js
+++ js/khusika.js
@@ -35,30 +35,3 @@ $('body').scrollspy({
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
-
-$(document).ready(function(){
-
-    $(".filter-button").click(function(){
-        var value = $(this).attr('data-filter');
-
-        if(value == "all")
-        {
-            //$('.filter').removeClass('hidden');
-            $('.filter').show('1000');
-        }
-        else
-        {
-//            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
-//            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
-            $(".filter").not('.'+value).hide('3000');
-            $('.filter').filter('.'+value).show('3000');
-
-        }
-    });
-
-    if ($(".filter-button").removeClass("active")) {
-$(this).removeClass("active");
-}
-$(this).addClass("active");
-
-});
