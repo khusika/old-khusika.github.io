4189ea565bd741776b0e8774a078eb7c769bb3c3
khusika <khusikadhamar@gmail.com>
2018-07-18 13:03:04 +0700

    Prevent user to save or print the page
    Signed-off-by: khusika <khusikadhamar@gmail.com>

--- js/khusika.js
+++ js/khusika.js
@@ -62,4 +62,10 @@ document.onkeydown = function(e) {
   if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
      return false;
   }
+  if(e.ctrlKey && e.keyCode == 'S'.charCodeAt(0)) {
+     return false;
+  }
+  if(e.ctrlKey && e.keyCode == 'P'.charCodeAt(0)) {
+     return false;
+  }
 }
