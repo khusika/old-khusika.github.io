110da9158131e7da1558fdc1a595af443ec01915
khusika <khusikadhamar@gmail.com>
2018-07-18 12:38:26 +0700

    Prevent visitor to open inspect element
    Signed-off-by: khusika <khusikadhamar@gmail.com>

--- js/khusika.js
+++ js/khusika.js
@@ -45,3 +45,21 @@ $(document).scroll(function() {
     $('.scroll-top').fadeOut();
   }
 });
+
+document.onkeydown = function(e) {
+  if(event.keyCode == 123) {
+     return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+     return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+     return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+     return false;
+  }
+  if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+     return false;
+  }
+}
